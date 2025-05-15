36a3628a9d380da7495f8d6adf28b4e4a9af8bf3
= <sba24003@cct.ie>
2025-05-15 22:39:40 +0100

    refactored gameBoard logic and tile to use data-row and data-col
    added forced hover styling, may need refactored.

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,28 @@
 const PORT = 3000;
 const url = "ws://localhost:" + PORT;
 const wsServer = new WebSocket(url);
-const displayChatsDiv = document.getElementsByClassName("game-tile");
+const gameTiles = document.getElementsByClassName("game-tile");
 
-const newMessageInput = document.getElementById("message");
-const authorInput = document.getElementById("author");
-function sendMessage() {
+console.log(gameTiles)
+
+function sendInput() {
   const data = {
-    
+    test: "test"
   };
 
   wsServer.send(JSON.stringify(data));
 }
-const sendMessageButton = document.getElementById("submit");
-sendMessageButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  sendMessage();
+
+gameTiles.addEventListener("click", (event) => {
+  sendInput();
 });
+
+gameTiles.addEventListener
 
 wsServer.onmessage = (wsRequest) => {
   console.log(wsRequest);
   const { data } = wsRequest;
-  // console.log(data);
+  console.log(data);
   displayNewMessage(JSON.parse(data));
 };
 
-function displayNewMessage(data) {
-  let newP = document.createElement("p");
-  newP.innerText = `Message from: ${data.author}\n${data.newMessage}`;
-  displayChatsDiv.appendChild(newP);
-}
--- routes/play.js
+++ routes/play.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const passport = require("../config/passportConfig");
 const router = express.Router();
-const gameConfig = require("../config/gameConfig")
+const { tileNxN } = require("../config/gameConfig");
 
 router.get("/", (req, res) => {
   if (!req.isAuthenticated()) return res.redirect("/login");
-  res.render("play.ejs", {gameConfig});
+  res.render("play.ejs", { tileNxN });
 });
 
 module.exports = router;
